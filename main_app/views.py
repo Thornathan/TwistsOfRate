71edbf8fb6fc1a85342dc3505f0ba09a92f7b24c
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-06-19 16:16:47 -0700

    Setup imports and finalized master branch before seperate branches

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import boto3
 # Create your views here.
 
 def home(request):
