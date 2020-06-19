ac2dd2ecbb8b0509a259a512b10fb7371514d85e
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-06-19 16:14:13 -0700

    Added base.html,about.html, and static/css/style.css

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
