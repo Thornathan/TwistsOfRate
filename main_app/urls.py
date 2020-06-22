25b1a6d98d0d5ce3c8e8e80ec0d6ffbf4b6d490d
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-06-22 10:14:10 -0700

    Update: base.html, urls.py, static images

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('console/', views.about, name='console'),
+    path('genre/', views.about, name='genre'),
+    path('title/', views.about, name='title'),
+    path('blog/', views.about, name='blog'),
 ]
